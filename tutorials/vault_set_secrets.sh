7f5bfb488f01cb34fd5a9abef0ef4de3238a2d4c
ignazio21 <ignaziopollina21@gmail.com>
2018-03-21 16:38:10 +0000

    mod

--- tutorials/vault_set_secrets.sh
+++ tutorials/vault_set_secrets.sh
@@ -3,8 +3,8 @@
 export VAULT_TOKEN=$1
 export VAULT_ADDR=$2
 
-execute_login=$(vault auth $VAULT_TOKEN)
+#execute_login=$(vault auth $VAULT_TOKEN)
 execute_read=$(vault read secret/password)
 
-echo $execute_login
+#echo $execute_login
 echo $execute_read
